fa2e763271ff83e1ee3af9b99fa06592c1281234
mmj030703 <mayankjain7738@gmail.com>
2024-03-30 23:30:54 +0530

    added additional screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'xs': '490px',
+      'sm': '640px',
+      'md': '768px',
+      'md-lg': '870px',
+      'lg': '1024px',
+      'xl': '1280px'
+    },
     extend: {},
     fontFamily: {
       poppins: ['Poppins', 'sans-serif'],
