fa3ee772e54dcd59a09bf2fde2a59b1d1592bd80
mmj030703 <mayankjain7738@gmail.com>
2024-03-30 23:30:20 +0530

    Added responsiveness to these components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,73 +1,80 @@
-import { useId } from "react";
+import { useId, useState } from "react";
 import Container from "./Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
+  const [showMobileNav, setShowMobileNav] = useState(false);
+
   const navItems = [
     {
       id: useId(),
       name: 'Home',
-      leftSide: true,
       active: true
     },
     {
       id: useId(),
       name: 'Services',
-      leftSide: true,
       active: false
     },
     {
       id: useId(),
       name: 'About',
-      leftSide: true,
       active: false
     },
     {
       id: useId(),
       name: 'Skills',
-      leftSide: true,
       active: false
     },
     {
       id: useId(),
       name: 'Projects',
-      leftSide: false,
       active: false
     },
     {
       id: useId(),
       name: 'Experience',
-      leftSide: false,
       active: false
     },
     {
       id: useId(),
       name: 'Testinomial',
-      leftSide: false,
       active: false
     },
     {
       id: useId(),
       name: 'Contact',
-      leftSide: false,
       active: false
     },
   ];
 
+  const showNavbar = () => {
+    setShowMobileNav((prevShowMobileNav) => {
+      return !prevShowMobileNav;
+    });
+  };
+
   return (
     <header>
       <Container>
-        <nav className="flex justify-center font-poppins">
-          <ul className="fixed z-50 mt-4 w-fit px-1 bg-zinc-900 text-white py-2 rounded-full flex justify-center mx-auto before before:absolute before:w-[2px] before:h-6 before:bg-zinc-700 before:-top-6 before:left-14 after:absolute after:h-6 after:w-[2px] after:bg-zinc-700 after:-top-6 after:right-14">
+        <nav className="relative flex justify-center font-poppins">
+          <ul className="fixed z-50 mt-4 w-fit px-1 bg-zinc-900 text-white py-2 rounded-full hidden md-lg:flex justify-center mx-auto before before:absolute before:w-[2px] before:h-6 before:bg-zinc-700 before:-top-6 before:left-14 after:absolute after:h-6 after:w-[2px] after:bg-zinc-700 after:-top-6 after:right-14">
             <div className="flex">
-              {navItems.map(navItem => navItem.leftSide && <li key={navItem.id} className={`${navItem.active ? 'bg-orange-500 rounded-full font-bold' : null} py-3 px-7 hover:bg-orange-500 hover:rounded-full cursor-pointer mx-1`}>{navItem.name}</li>)}
-            </div>
-            <div className="flex items-center mx-12 cursor-pointer">
-              <h2 className="font-bold font-pacifico text-3xl">Portfolio</h2>
-            </div>
-            <div className="flex">
-              {navItems.map(navItem => !navItem.leftSide && <li key={navItem.id} className={`${navItem.active ? 'bg-orange-500 rounded-full font-bold' : null} py-3 px-7 hover:bg-orange-500 hover:rounded-full cursor-pointer mx-1`}>{navItem.name}</li>)}
+              {navItems.map(navItem => <li key={navItem.id} className={`${navItem.active ? 'bg-orange-500 rounded-full font-bold' : null}  px-3 lg:max-xl:px-5 xl:px-7 py-2 xl:py-4 hover:bg-orange-500 hover:rounded-full cursor-pointer mx-1 text-[15px] xl:text-base`}>{navItem.name}</li>)}
             </div>
           </ul>
+          { /* Mobile Navbar */}
+          <FontAwesomeIcon onClick={showNavbar} icon={faBars} className="fixed z-50 bg-orange-400 py-[14px] px-4 rounded-full block md-lg:hidden right-2 xs:right-4 sm:right-7 top-7 sm:top-6 cursor-pointer text-2xl sm:text-3xl" />
+          {
+            (
+              <ul className={`fixed ${showMobileNav ? 'top-[0]' : 'top-[-650px]'} border-2 border-orange-400 transition-all duration-1000 ease-in-out rounded-2xl w-[95%] mx-auto z-40 px-2 pt-4 pb-3 mt-4 bg-zinc-900 text-white justify-center before before:absolute before:w-[2px] before:h-6 before:bg-zinc-700 before:-top-6 before:left-14 after:absolute after:h-6 after:w-[2px] after:bg-zinc-700 after:-top-6 after:right-14`}>
+                <div className="flex flex-col gap-y-[4px] pt-14">
+                  {navItems.map(navItem => <li key={navItem.id} className={`${navItem.active ? 'bg-orange-500 rounded-full font-bold' : null}  px-3 lg:max-xl:px-5 xl:px-7 py-2 xl:py-4 hover:bg-orange-500 hover:rounded-full cursor-pointer mx-1 text-[15px] xl:text-base`}>{navItem.name}</li>)}
+                </div>
+              </ul>
+            )
+          }
         </nav>
       </Container>
     </header>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-import { faArrowRight, faQuoteLeft, faStar } from '@fortawesome/free-solid-svg-icons';
+import { faQuoteLeft, faStar } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useSelector } from 'react-redux';
 
@@ -8,32 +8,28 @@ function Hero() {
 
   const stars = [];
   for (let i = 0; i < 5; i++) {
-    stars.push(<FontAwesomeIcon key={Math.random()*10+1} icon={faStar} className='text-amber-600 text-[1.3rem]' />);
+    stars.push(<FontAwesomeIcon key={Math.random() * 10 + 1} icon={faStar} className='text-amber-600 text-[1.3rem]' />);
   }
-  
+
   return (
     <section>
-      <div className='mt-28'>
+      <div className='mt-10 md-lg:mt-28 px-5 md-lg:px-0'>
         <div>
           <p className='font-semibold text-lg border-2 border-black w-fit mx-auto rounded-full py-[6px] px-5 text-center'><span>Hello!</span></p>
-          <h1 className='text-[5rem] font-semibold tracking-tight text-center'>I&apos;m <span className='text-orange-500'>{user?.about?.name}</span>,<span className='block -mt-8'>{user?.about?.title}</span></h1>
+          <h1 className='text-[2.3rem] xs:text-[2.8rem] sm:text-[3.5rem] md-lg:text-[5rem] font-bold sm:font-semibold tracking-tight text-center'>I&apos;m <span className='text-orange-500'>{user?.about?.name}</span>,<span className='max-xs:text-[1.5rem] block -mt-3 xs:-mt-5 sm:-mt-8'>{user?.about?.title}</span></h1>
         </div>
-        <div className='flex gap-x-28 justify-evenly'>
-          <div className='mt-10'>
+        <div className='flex md:gap-x-14 md-lg:gap-x-28 justify-evenly'>
+          <div className='hidden md:block mt-10 px-10'>
             <FontAwesomeIcon icon={faQuoteLeft} className='text-4xl' />
-            <p className='max-w-[300px] font-semibold text-[19.5px] mt-2'>{user?.about?.subTitle}</p>
+            <p className='max-w-[300px] font-semibold text-[17px] md-lg:text-[19.5px] mt-2'>{user?.about?.subTitle}</p>
           </div>
-          <div className='relative -ms-[8rem] -mt-12'>
-            <img className='-z-20 w-96 rounded-t-full' src={user?.about?.avatar?.url} alt='Profile Image' />
-            <div className='flex gap-x-5 items-center absolute bottom-7 left-[12%] text-lg text-white border-2 border-white w-fit mx-auto rounded-full py-2 px-2 text-center'>
-              <button className='w-max bg-orange-500 rounded-full py-3 px-7 text-xl font-medium'>Portfolio <FontAwesomeIcon icon={faArrowRight} className='-rotate-45 ms-1' /></button>
-              <span className='w-max font-medium pe-1'>Hire Me</span>
-            </div>
+          <div className='relative md:me-14 md-lg:me-0 md-lg:-ms-[8rem] xs:-mt-7 sm:-mt-9 md:-mt-12'>
+            <img className='-z-20 w-60 sm:w-96 rounded-t-full' src={user?.about?.avatar?.url} alt='Profile Image' />
           </div>
-          <div className='mt-10'>
+          <div className='hidden md:block mt-10 px-5'>
             {stars}
-            <p className='font-bold mt-2 text-4xl tracking-wide'>{user?.about?.exp_year} Years</p>
-            <p className='font-medium text-lg'>Experience</p>
+            <p className='w-max font-bold mt-2 text-3xl md-lg:text-4xl tracking-wide'>{user?.about?.exp_year} Years</p>
+            <p className='w-max font-medium text-base md-lg:text-lg'>Experience</p>
           </div>
         </div>
       </div>
