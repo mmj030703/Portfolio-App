b25279dcf0ce5b45b35e922e045db78afe8e7612
mmj030703 <mayankjain7738@gmail.com>
2024-03-30 23:28:30 +0530

    Added Auto Slider to the SkillsSlider component

--- src/components/SkillsSlider.jsx
+++ src/components/SkillsSlider.jsx
@@ -5,11 +5,14 @@ import { faStar } from "@fortawesome/free-solid-svg-icons";
 function SkillsSlider() {
     const portfolioDetails = useSelector(store => store?.portfolioDetails?.portfolioDetails);
     const skillList = portfolioDetails?.user?.skills;
-
+    
     return (
-        <section className="mt-20 bg-orange-500 pt-0 pb-[4rem] rounded-tl-[27px] rounded-br-[20px] overflow-hidden">
-            <div className="w-max bg-white mb-3 flex gap-x-10 -rotate-[1.8deg] font-semibold text-4xl py-2 list-none">
-                {skillList?.map(skill => <span key={skill?._id}><FontAwesomeIcon icon={faStar} className="text-3xl me-5 text-orange-500" /> {skill.name}</span>)}
+        <section className="mt-20 bg-orange-500 pt-[2.5rem] pb-[1rem] rounded-tl-[20px] flex rounded-br-[14px] overflow-hidden">
+            <div className="animate-slider pe-5 w-max bg-white mb-3 flex gap-x-10  font-semibold text-4xl py-5 list-none">
+                {skillList?.map(skill => <span key={skill?._id} className="w-max"><FontAwesomeIcon icon={faStar} className="text-4xl me-5 text-orange-500" /> {skill.name}</span>)}
+            </div>
+            <div className="animate-slider ps-5 w-max bg-white mb-3 flex gap-x-10 font-semibold text-4xl py-5 list-none">
+                {skillList?.map(skill => <span key={skill?._id} className="w-max"><FontAwesomeIcon icon={faStar} className="text-4xl me-5 text-orange-500" /> {skill.name}</span>)}
             </div>
         </section>
     )
