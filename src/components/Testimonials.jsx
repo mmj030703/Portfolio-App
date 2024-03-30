5fdf66b9c76b739fec3270a0333ce0d4b69b355c
mmj030703 <mayankjain7738@gmail.com>
2024-03-30 23:29:46 +0530

    Updated the code

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,19 +2,19 @@ import { faQuoteRightAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useSelector } from "react-redux";
 
-function Testinomials() {
+function Testimonials() {
     const portfolioDetails = useSelector(store => store?.portfolioDetails?.portfolioDetails);
     const testimonials = portfolioDetails?.user?.testimonials;
 
     return (
-        <section className='mt-20 bg-svg w-full h-[600px] block rounded-[2.5rem] overflow-hidden'>
+        <section className='mt-20 bg-svg w-full h-[600px] block rounded-[2.5rem]'>
             <div className='w-full pt-20 flex flex-col items-center'>
                 <h1 className='text-4xl font-bold text-center text-white max-w-96'>Testinomials That Speak to <span className='text-orange-400'>My Results</span></h1>
-                <div className="flex gap-x-10 mt-20">
+                <div className="scroll-smooth flex w-full gap-x-10 px-6 mt-20 overflow-hidden">
                     {
                         testimonials?.map(testimonial => testimonial?.enabled && (
-                            <div key={testimonial?._id} className="w-[700px] ps-5 pe-3 pt-1 pb-0 bg-white/20 rounded-2xl">
-                                <div className="flex justify-between items-center">
+                            <div key={testimonial?._id} className="ps-5 pe-3 pt-1 pb-5 bg-white/20 rounded-2xl">
+                                <div className="w-[700px] flex justify-between items-center">
                                     <div className="flex gap-x-2 items-center">
                                         <img src={testimonial?.image?.url} className="w-10 rounded-full" alt="Testinomial Image" />
                                         <article>
@@ -34,4 +34,4 @@ function Testinomials() {
     )
 }
 
-export default Testinomials;
+export default Testimonials;
